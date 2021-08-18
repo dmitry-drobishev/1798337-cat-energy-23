bad60727140752996e79cc973aa2e7fafecf83e2
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2021-08-18 18:10:55 +0300

    Декоративные элементы главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
-// navToggle
+//main navigation
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.page-header__toggle');
+
   navMain.classList.remove('main-nav--nojs');
   navToggle.classList.remove('page-header__toggle--nojs');
 
@@ -18,31 +19,29 @@ const navToggle = document.querySelector('.page-header__toggle');
     }
   });
 
-      // example__slider-button--before
-      // example__slider-handle--before
-      // example__slide--before
 
 
-const slideBefore = document.querySelector('example__slide--before');
-const slideAfter = document.querySelector('example__slide--after');
-const sliderHandle = document.querySelector('example__slider-handle');
-const buttonBefore = document.querySelector('example__slider-button--before');
-const buttonAfter = document.querySelector('example__slider-button--after');
 
-buttonBefore.addEventListener('click', function() {
-  if (sliderHandle.classList.contains('example__slider-handle--after')) {
-    slideBefore.classList.add('example__slide--active');
-    slideAfter.classList.remove('example__slide--active');
-    sliderHandle.classList.add('example__slider-handle--before');
-    sliderHandle.classList.remove('example__slider-handle--after');
-  }
-});
+// const slideBefore = document.querySelector('example__slide--before');
+// const slideAfter = document.querySelector('example__slide--after');
+// const sliderHandle = document.querySelector('example__slider-handle');
+// const buttonBefore = document.querySelector('example__slider-button--before');
+// const buttonAfter = document.querySelector('example__slider-button--after');
 
-buttonAfter.addEventListener('click', function() {
-  if (sliderHandle.classList.contains('example__slider-handle--before')) {
-    slideBefore.classList.remove('example__slide--active');
-    slideAfter.classList.add('example__slide--active');
-    sliderHandle.classList.remove('example__slider-handle--before');
-    sliderHandle.classList.add('example__slider-handle--after');
-  }
-});
+// buttonBefore.addEventListener('click', function() {
+//   if (sliderHandle.classList.contains('example__slider-handle--after')) {
+//     slideBefore.classList.add('example__slide--active');
+//     slideAfter.classList.remove('example__slide--active');
+//     sliderHandle.classList.add('example__slider-handle--before');
+//     sliderHandle.classList.remove('example__slider-handle--after');
+//   }
+// });
+
+// buttonAfter.addEventListener('click', function() {
+//   if (sliderHandle.classList.contains('example__slider-handle--before')) {
+//     slideBefore.classList.remove('example__slide--active');
+//     slideAfter.classList.add('example__slide--active');
+//     sliderHandle.classList.remove('example__slider-handle--before');
+//     sliderHandle.classList.add('example__slider-handle--after');
+//   }
+// });
